3a968b359994d78f6b839b9d84eeb2ada9f3b166
CrawfordBrown <crawford.brown1@outlook.com>
2022-03-17 14:03:16 +0000

    Git issue resolved

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -117,9 +117,153 @@ public class App
 
     }
 
-    /*
-    All the cities in the world organised by largest population to smallest.
+     /*
+    All the countries in a continent organised by largest population to smallest
      */
+
+    private void report2(String cont) {
+
+        System.out.println("All the countries in a continent organised by largest population to smallest");
+        StringBuilder sb  = new StringBuilder();
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql2 = "select * " +
+                    "from country " +
+                    "where Continent = " + cont +
+                    "Order By Population desc;";
+            // Execute SQL statement
+            ResultSet rset2 = stmt.executeQuery(sql2);
+            // Return new country if valid.
+            // Check one is returned
+            while (rset2.next()) {
+                String code = rset2.getString("code");
+                String name = rset2.getString("name");
+                String continent = rset2.getString("continent");
+                String region = rset2.getString("region");
+                Integer surfaceArea = rset2.getInt("surfaceArea");
+                Integer indepYear = rset2.getInt("indepYear");
+                Integer population = rset2.getInt("population");
+                Integer lifeExpectancy = rset2.getInt("lifeExpectancy");
+                Integer gnp = rset2.getInt("gnp");
+                Integer gnpOld = rset2.getInt("gnpOld");
+                String localName = rset2.getString("localName");
+                String governmentForm = rset2.getString("governmentForm");
+                String headOfState = rset2.getString("headOfState");
+                Integer capital = rset2.getInt("capital");
+                String code2 = rset2.getString("code2");
+                Country country = new Country(code, name, continent, region, surfaceArea, indepYear, population,
+                        lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2);
+                sb.append(country.toString() + "\r\n");
+            }
+            // Displays the records
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return;
+        }
+
+    }
+
+
+    //All the countries in a continent organised by largest population to smallest
+
+    private void report3(String reg) {
+
+        System.out.println("All the countries in a Region organised by largest population to smallest");
+        StringBuilder sb  = new StringBuilder();
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql3 = "select * " +
+                    "from country " +
+                    "where Region = " + reg +
+                    "Order By Population desc;";
+            // Execute SQL statement
+            ResultSet rset3 = stmt.executeQuery(sql3);
+            // Return new country if valid.
+            // Check one is returned
+            while (rset3.next()) {
+                String code = rset3.getString("code");
+                String name = rset3.getString("name");
+                String continent = rset3.getString("continent");
+                String region = rset3.getString("region");
+                Integer surfaceArea = rset3.getInt("surfaceArea");
+                Integer indepYear = rset3.getInt("indepYear");
+                Integer population = rset3.getInt("population");
+                Integer lifeExpectancy = rset3.getInt("lifeExpectancy");
+                Integer gnp = rset3.getInt("gnp");
+                Integer gnpOld = rset3.getInt("gnpOld");
+                String localName = rset3.getString("localName");
+                String governmentForm = rset3.getString("governmentForm");
+                String headOfState = rset3.getString("headOfState");
+                Integer capital = rset3.getInt("capital");
+                String code2 = rset3.getString("code2");
+                Country country = new Country(code, name, continent, region, surfaceArea, indepYear, population,
+                        lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2);
+                sb.append(country.toString() + "\r\n");
+            }
+            // Displays the records
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return;
+        }
+
+    }
+
+    //Top n populated countries
+
+    private void report4(int num) {
+
+        System.out.println("Top n populated countries");
+        StringBuilder sb  = new StringBuilder();
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql4 = "select * " +
+                    "from country " +
+                    "Order By Population desc Limit " + num + ";";
+            // Execute SQL statement
+            ResultSet rset4 = stmt.executeQuery(sql4);
+            // Return new country if valid.
+            // Check one is returned
+            while (rset4.next()) {
+                String code = rset4.getString("code");
+                String name = rset4.getString("name");
+                String continent = rset4.getString("continent");
+                String region = rset4.getString("region");
+                Integer surfaceArea = rset4.getInt("surfaceArea");
+                Integer indepYear = rset4.getInt("indepYear");
+                Integer population = rset4.getInt("population");
+                Integer lifeExpectancy = rset4.getInt("lifeExpectancy");
+                Integer gnp = rset4.getInt("gnp");
+                Integer gnpOld = rset4.getInt("gnpOld");
+                String localName = rset4.getString("localName");
+                String governmentForm = rset4.getString("governmentForm");
+                String headOfState = rset4.getString("headOfState");
+                Integer capital = rset4.getInt("capital");
+                String code2 = rset4.getString("code2");
+                Country country = new Country(code, name, continent, region, surfaceArea, indepYear, population,
+                        lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2);
+                sb.append(country.toString() + "\r\n");
+            }
+            // Displays the records
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return;
+        }
+
+    }
+
+
 
     private void report5() {
 
@@ -322,6 +466,9 @@ public class App
 
         // Display the Records
         a.report1();
+        a.report2("'Asia'");
+        a.report3("'Caribbean'");
+        a.report4(5);
 
         // Display all the cities in the world organised by largest population to smallest.
         a.report5();
@@ -343,3 +490,5 @@ public class App
     }
 
 }
+
+
